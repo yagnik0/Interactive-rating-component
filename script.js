23d02eaeea850074a9eb9853f64fc8f4d064bc34
yagnik0 <yashyagnik456@.com>
2023-12-03 21:46:37 +0530

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const ratingCard = document.querySelector('.rating-container')
+const thankCard = document.querySelector('.thankyou-container')
+const submitButton = document.querySelector('.submit-btn')
+const rating = document.getElementById('rating')
+const ratingNumber = document.querySelectorAll('.btn')
+
+submitButton.addEventListener('click',() => {
+    thankCard.classList.remove('hidden')
+    ratingCard.style.display = "none"
+})
+
+ratingNumber.forEach((rates) => {
+    rates.addEventListener('click', () => {
+        rating.innerHTML = rates.innerHTML
+    })
+})
+
+
+
+
+
+
+
 
 
 
